7e830e726faa72346cfa5ac14f0a2e56f64ccaab
jamie connell <Jamie.Connell_10@hotmail.com>
2024-01-31 20:45:25 +0000

    settings.py( import dj_database_url)

--- gem_places/settings.py
+++ gem_places/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l#wl!yx-9o7@hu40uqbg-7%l!!(-!@zg=pt*oex+sd^$5f100*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ '127.0.0.1',
                 '.herokuapp.com']
