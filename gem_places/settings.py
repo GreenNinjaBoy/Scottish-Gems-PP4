9a14fb46ce220b4f0d63f918a06aaf1063c74a3a
jamie connell <Jamie.Connell_10@hotmail.com>
2024-02-01 19:53:58 +0000

    test

--- gem_places/settings.py
+++ gem_places/settings.py
@@ -79,13 +79,9 @@ WSGI_APPLICATION = 'gem_places.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
