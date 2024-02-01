cdc0de3ca818252a8e4aad9cdadfe00922f1dca0
jamie connell <Jamie.Connell_10@hotmail.com>
2024-02-01 21:11:50 +0000

    models.py (add image content to post model)

--- scottish_gems/models.py
+++ scottish_gems/models.py
@@ -9,8 +9,12 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
+        User, on_delete=models.CASCADE, related_name="gem_places_posts"
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True) # Will add the date that the post was created
     STATUS = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    image = models.ImageField(upload_to='post_images', blank=True)
+
+
