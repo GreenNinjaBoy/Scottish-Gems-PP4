daaac53488bc9a9c97923cee0833594c6ed3c180
jamie connell <Jamie.Connell_10@hotmail.com>
2024-02-01 20:36:03 +0000

    models.py:(create post model)

--- scottish_gems/models.py
+++ scottish_gems/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "published"))
+
+# creating the post model
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True) # Will add the date that the post was created
+    STATUS = models.IntegerField(choices=STATUS, default=0)
