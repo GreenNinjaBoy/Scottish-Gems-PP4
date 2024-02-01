46ec68651988da03c3099f50c8eec59fefb51cfb
jamie connell <Jamie.Connell_10@hotmail.com>
2024-02-01 20:10:32 +0000

    env.py( add SECRET_KEY to file and heroku)

--- gem_places/settings.py
+++ gem_places/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l#wl!yx-9o7@hu40uqbg-7%l!!(-!@zg=pt*oex+sd^$5f100*'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ '127.0.0.1',
                 '.herokuapp.com',
