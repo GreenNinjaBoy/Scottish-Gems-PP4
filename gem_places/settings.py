6b36a07608dd643d745b85ac57b12870a32a7438
jamie connell <Jamie.Connell_10@hotmail.com>
2024-02-04 15:53:38 +0000

    admin.py( post model now showing on admin page)

--- gem_places/settings.py
+++ gem_places/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ '127.0.0.1',
                 '.herokuapp.com',
@@ -87,7 +87,7 @@ DATABASES = {
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
     "https://*.herokuapp.com",
-    "http://*.gitpod.io"
+    "https://*.gitpod.io"
 ]
 
 
--- scottish_gems/models.py
+++ scottish_gems/models.py
@@ -9,12 +9,13 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="gem_places_posts"
+        User, on_delete=models.CASCADE, related_name="gem_places"
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True) # Will add the date that the post was created
     STATUS = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     image = models.ImageField(upload_to='post_images', blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
 
