ce0c625ce96ae60898712fd73d94eba4aa1ec8b3
jamie connell <Jamie.Connell_10@hotmail.com>
2024-02-04 15:19:35 +0000

    settings.py(add CSRF_TRUSTED_ORIGINS)

--- gem_places/settings.py
+++ gem_places/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ '127.0.0.1',
                 '.herokuapp.com',
@@ -83,6 +83,12 @@ WSGI_APPLICATION = 'gem_places.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com",
+    "http://*.gitpod.io"
+]
 
 
 # Password validation
