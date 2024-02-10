6540b690de444008b54ce5397df7b8e7783fc27a
jamie connell <Jamie.Connell_10@hotmail.com>
2024-02-10 15:51:54 +0000

    signup.html(transform signup.html to signup_login.html)

--- scottish_gems/urls.py
+++ scottish_gems/urls.py
@@ -1,7 +1,8 @@
 from . import views
+from .views import signup_login
 from django.urls import path
 
 urlpatterns = [
     path('', views.GemList.as_view(), name='home'),
-    path('signup/', views.signup, name='signup'),
+    path('signup_login/', signup_login, name='signup_login'),
 ]
--- scottish_gems/views.py
+++ scottish_gems/views.py
@@ -1,19 +1,39 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, User
 
-def signup(request):
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+
+def signup_login(request):
+    signup_form = UserCreationForm()
+    login_form = AuthenticationForm()
+
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'accounts/signup.html', {'form': form})
+        if 'signup' in request.POST:
+            signup_form = UserCreationForm(request.POST)
+            if signup_form.is_valid():
+                signup_form.save()
+                # handle successful signup
+        elif 'login' in request.POST:
+            login_form = AuthenticationForm(data=request.POST)
+            if login_form.is_valid():
+                # handle successful login
+                pass
 
+    return render(request, 'accounts/signup_login.html', {'signup_form': signup_form, 'login_form': login_form})
 
 # Create your views here.
 
