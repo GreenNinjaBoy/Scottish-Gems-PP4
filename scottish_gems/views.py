9695ae125c475d82f7f5eb6f70bce14d6a53d8f6
jamie connell <Jamie.Connell_10@hotmail.com>
2024-02-10 15:54:42 +0000

    signup.html(transform signup.html to signup_login.html)

--- scottish_gems/views.py
+++ scottish_gems/views.py
@@ -3,20 +3,6 @@ from django.views import generic
 from .models import Post
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, User
 
-class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ("username", "email", "password1", "password2")
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.email = self.cleaned_data["email"]
-        if commit:
-            user.save()
-        return user
-
 def signup_login(request):
     signup_form = UserCreationForm()
     login_form = AuthenticationForm()
