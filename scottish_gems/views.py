1881f2628b0b9f9c6c06c605273e97e6cfe31a15
jamie connell <Jamie.Connell_10@hotmail.com>
2024-02-23 13:22:01 +0000

    views.py(tidy code to look cleaner)

--- scottish_gems/views.py
+++ scottish_gems/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views import generic
 from .models import Post, Region, UserComments
 from django.template.loader import render_to_string
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
@@ -18,12 +17,11 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            auth_login(request, user)  # Log the user in
-            return redirect('home')  # Redirect to home page after successful signup
+            login(request, user)
+            return redirect('home')
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
-
 
 def login_view(request):
     if request.method == 'POST':
@@ -34,33 +32,26 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('home')  # Redirect to home page
+                return redirect('home')
     else:
         form = AuthenticationForm()
-    return render(request, 'accounts/login.html', {'login_form': form})  # Pass the form as 'login_form'
+    return render(request, 'accounts/login.html', {'login_form': form})
 
 def logout_view(request):
     if request.method == 'POST':
         logout(request)
-        # Read the message from the file
         message = render_to_string('accounts/messages/logged_out.txt')
-        # Add the message
         messages.success(request, message)
-        # Redirect to the base page
-        return redirect('base')
+        return redirect('home')
     else:
-        # If the request is not a POST request, render the logout confirmation page
         return render(request, 'accounts/logout.html')
 
-def gem_detail (request, post_id):
+def gem_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     comments = post.comments.all()
-    print(f"Comments for post {post_id}: {comments}")  # Debug print
     if request.method == 'POST':
         new_comment = UserComments.objects.create(comment=request.POST['content'], author=request.user, place=post)
-        print(f"New comment: {new_comment.comment}")  # Debug print
     return render(request, 'gem_posts/gem_detail.html', {'post': post, 'comments': comments})
-
 
 def posts_by_region(request, region_id):
     region = get_object_or_404(Region, id=region_id)
@@ -68,30 +59,23 @@ def posts_by_region(request, region_id):
     regions = Region.objects.all()
     return render(request, 'base.html', {'posts': posts, 'regions': regions, 'region_selected': True})
 
-    # Get all posts in this region
-    posts = Post.objects.filter(region=region)
-
-    # Render the template with the posts
-    return render(request, 'base.html', {'posts': posts})
-
-def base_view(request):
-    # Render the base template
-    return render(request, 'base.html')
-
-@login_required
-def toggle_favorite(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    if post in request.user.favorite_posts.all():
-        request.user.favorite_posts.remove(post)
-    else:
-        request.user.favorite_posts.add(post)
-    return redirect('post_detail', post_id=post.id)
 
 @login_required
 def favorites(request):
     favorite_posts = request.user.favorite_posts.all()
     return render(request, 'accounts/favorites.html', {'favorite_posts': favorite_posts})
 
+@login_required
+def toggle_favorite(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if post in request.user.favorite_posts.all():
+        request.user.favorite_posts.remove(post)
+        messages.success(request, 'Post removed from favorites.')
+    else:
+        request.user.favorite_posts.add(post)
+        messages.success(request, 'Post added to favorites.')
+    return redirect('home')
+    
 @login_required
 def toggle_favorite(request, post_id):
     post = get_object_or_404(Post, id=post_id)
@@ -152,10 +136,3 @@ def delete_comment(request, post_id, comment_id):
         comment.delete()
         return redirect('gem_detail', post_id=post.id)
     return render(request, 'gem_posts/delete_comment.html', {'comment': comment})
-
-class GemList(generic.ListView):
-    queryset = Post.objects.filter(STATUS=1)
-    template_name = 'scottish_gems/index.html'
-    paginate_by = 6
-    
-    
