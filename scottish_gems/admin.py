848e78f44bb58b13618c6435a87dcde0571c6a2c
jamie connell <Jamie.Connell_10@hotmail.com>
2024-02-28 19:51:44 +0000

    admin.py( add docstring comment to code)

--- scottish_gems/admin.py
+++ scottish_gems/admin.py
@@ -5,12 +5,20 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    """
+    Admin view for the Post model. It uses the Summernote WYSIWYG editor for the 
+    content field. The admin list view displays the title, slug, and creation date 
+    of each post. It also provides search functionality for the title and content 
+    fields, and allows filtering posts by creation date. The slug field is 
+    automatically populated based on the title field.
+    """
     list_display = ('title', 'slug', 'created_on')
     search_fields = ['title', 'content']
     list_filter = ( 'created_on',)
     prepopulated_fields = {'slug':('title',)}
     summernotes_fields = {'content',}
 
-# Register your models here.
+# Register the UserComments and Region models with the admin site.
+
 admin.site.register(UserComments)
 admin.site.register(Region)
