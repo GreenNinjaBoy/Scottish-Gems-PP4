500c445feee7381f5bdcec73c10a978f4d89e14a
jamie connell <Jamie.Connell_10@hotmail.com>
2024-02-29 10:20:28 +0000

    500 error (fix 500 error)

---
+++ staticfiles/js/add-gem-form.6193daa6dea1.js
@@ -0,0 +1,76 @@
+// Get references to the HTML elements that will be used in the script.
+const searchInput = document.getElementById("search-input");
+const latitudeField = document.getElementById("latitude-field");
+const longitudeField = document.getElementById("longitude-field");
+const googlePlaceIdField = document.getElementById("googlePlaceIdField");
+const nameField = document.getElementById("add-form-title");
+const addressField = document.getElementById("add-form-address");
+
+// Get references to the hidden fields in the form.
+const titleField = document.getElementById("title-field");
+const addressFieldForm = document.getElementById("address-field");
+const contentField = document.getElementById("content-field");
+
+// Function to initialize the Google Places autocomplete functionality.
+function initAutocomplete() {
+    // Define the geographical bounds for the autocomplete search.
+    const SCOTLAND_BOUNDS = {
+        north: 60.85,
+        south: 54.60,
+        west: -8.65,
+        east: -0.76,
+    };
+    // Set the options for the autocomplete search.
+    const options = {
+        bounds: SCOTLAND_BOUNDS,
+        componentRestrictions: {
+            country: ["uk"]
+        },
+        fields: ["formatted_address", "geometry", "name", "photos", "place_id"],
+        strictBounds: true,
+    };
+     // Create the autocomplete object.
+    const autocomplete = new google.maps.places.Autocomplete(searchInput, options);
+
+
+     // Event listener for when a place is selected from the autocomplete dropdown.
+    autocomplete.addListener("place_changed", () => {
+        // Get the selected place.
+        const place = autocomplete.getPlace();
+        
+        // Get the geographical coordinates of the selected place.
+        const coordinates = place.geometry.location;
+        const latitude = coordinates.lat();
+        const longitude = coordinates.lng();
+        
+        // Update the text content and value of the form fields with the details of the selected place.
+        nameField.textContent = place.name;
+        titleField.value = place.name;  
+        latitudeField.value = latitude;
+        longitudeField.value = longitude;
+        googlePlaceIdField.value = place.place_id;
+        addressField.textContent = place.formatted_address;
+        addressFieldForm.value = place.formatted_address;  
+        contentField.value = place.formatted_address;  
+
+        // Get a single photo URL from Google Places.
+        if (place.photos && place.photos.length > 0) {
+            const photoUrl = place.photos[0].getUrl({
+                maxHeight: 750, maxWidth: 750,
+            });
+             // If the selected place has photos, get the URL of the first photo and update the src attribute of the img element.
+            const imgElement = document.getElementById("photo");
+            imgElement.src = photoUrl;
+            imgElement.alt = place.name + " Photo";
+
+            const photoUrlField = document.getElementById("photoUrlField");
+            photoUrlField.value = photoUrl;
+
+        }
+
+        // Display the form for adding a new place.
+        const placeAddForm = document.getElementById("place-add-form");
+
+        placeAddForm.style.display = "flex";
+    });
+}
---
+++ staticfiles/js/comment.js
@@ -0,0 +1,21 @@
+$('.delete-comment').on('click', function() {
+    // Get the comment ID from the data attribute
+    var commentId = $(this).data('comment-id');
+    // Confirm the deletion
+    var confirmation = confirm('Are you sure you want to delete this comment?');
+    // If the user confirmed the deletion
+    if (confirmation) {
+        // Send a POST request to the server to delete the comment
+        $.ajax({
+            url: '/post/' + $(this).data('post-id') + '/comment/' + commentId + '/delete/',
+            type: 'POST',
+            data: {
+                'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val()
+            },
+            success: function() {
+                // Reload the page
+                location.reload();
+            }
+        });
+    }
+});
