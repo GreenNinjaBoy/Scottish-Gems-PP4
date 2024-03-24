f1ff41deeab5a8b3d63ba44244871b51b0e7e597
jamie connell <Jamie.Connell_10@hotmail.com>
2024-03-24 20:47:43 +0000

    navbar.html(fix navbar style)

---
+++ static/js/nav-bar.js
@@ -0,0 +1,10 @@
+function toggleMenuIcon(element) {
+    const icon = element.querySelector('i');
+    if (icon.classList.contains('fa-bars')) {
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
+    } else {
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+    }
+}
